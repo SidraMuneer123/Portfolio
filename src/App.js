8b84bef19bd6716102c044358bff9e9b5ff2ba88
New-repo <sidra182001@gmail.com>
2023-10-09 20:08:44 +0500

    Adding Services and freelancing section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,10 @@ import Navbar from './components/Navbar';
 import {BrowserRouter as Router , Route , Routes} from 'react-router-dom';
 import About from './components/About';
 import Home from './components/Home';
+import Services from './components/Services';
+import Freelancing from './components/Freelancing';
+
+
 
 
 const App = () => {
@@ -11,6 +15,9 @@ const App = () => {
     <Navbar  />
     <Home />
     <About />
+    <Services />
+    <Freelancing />
+    
 
       
     </div>
---
+++ src/components/Freelancing.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Freelancing = () => {
+  return (
+    <>
+        <div className='bg-gray-100 h-16 mb-8'>
+    <div class="relative">
+        
+        <img src="/Images/fresh.jpg" alt="Image" class="w-full filter brightness-50 h-72" />
+
+         
+        <div class="absolute top-0 left-0 w-full h-full flex items-center justify-center">
+            <div class="bg-transparent bg-opacity-75 p-4 rounded-lg shadow-md">
+                <h1 class="lg:text-6xl sm:text-4xl text-2xl font-semibold text-blue-200">I Am Available For Freelancing</h1>
+                {/* <p class="text-md text-gray-700">Your description goes here</p> */}
+            </div>
+        </div>
+    </div>
+
+    </div>
+    </>
+  )
+}
+
+export default Freelancing
